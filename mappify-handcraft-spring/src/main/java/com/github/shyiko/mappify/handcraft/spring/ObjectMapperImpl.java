6af7e280a8285cbe75859f10f99f534c3315507a
Stanley Shyiko <stanley.shyiko@gmail.com>
2012-10-19 09:25:47 +0300

    Renamed module mappify-bare-spring to mappify-handcraft-spring

--- mappify-handcraft-spring/src/main/java/com/github/shyiko/mappify/handcraft/spring/ObjectMapperImpl.java
+++ mappify-handcraft-spring/src/main/java/com/github/shyiko/mappify/handcraft/spring/ObjectMapperImpl.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.github.shyiko.mappigy.bare.spring;
+package com.github.shyiko.mappify.handcraft.spring;
 
 import com.github.shyiko.mappify.api.AbstractObjectMapper;
 import com.github.shyiko.mappify.api.MappingContext;
